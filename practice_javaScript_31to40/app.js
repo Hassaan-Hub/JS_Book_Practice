d2b281aaf28321089af07bf4b522c3d863be8306
Hassaan Farid <hassaanali994@gmail.com>
2026-01-21 22:26:43 +0500

    updated

---
+++ practice_javaScript_31to40/app.js
@@ -0,0 +1,60 @@
+// 31 Getting the current date and time
+
+
+// var rightNow = new Date();
+
+// console.log(rightNow);
+
+
+
+
+// 32 Extracting parts of the date and time
+
+
+// var rightNow = new Date()
+// var day = rightNow.getDay()
+// var month = rightNow.getMonth()
+// var date = rightNow.getDate()
+// var year = rightNow.getFullYear()
+// var hour = rightNow.getHours()
+// var minute = rightNow.getMinutes()
+// var second = rightNow.getSeconds()
+// var miliseconds = rightNow.getMilliseconds()
+// var time = rightNow.getTime()
+
+// console.log(month);
+
+
+
+
+// 33 Specifying a date and time
+
+
+// var current = new Date()
+// var selected = new Date("Nov 18, 2007")
+// var currentTime = current.getTime()
+// var selectedTime = selected.getTime()
+// var minus = currentTime - selectedTime
+// var result = minus / (1000 * 60 * 60 * 24)
+
+// console.log(result);
+
+
+
+
+// 34 Changing elements of a date and time
+
+
+var date = new Date()
+var fu = date.setFullYear(2007)
+console.log(`year is ${fu}`);
+
+
+
+
+
+
+
+
+
+
