b1ea9ef548dc0fcbfd5b5e6439a3bce505fa2338
Hassaan Farid <hassaanali994@gmail.com>
2026-01-22 21:13:49 +0500

    refactor: clean up commented code and add timer function in app.js

--- practice_javaScript_31to40/app.js
+++ practice_javaScript_31to40/app.js
@@ -1,15 +1,10 @@
 // 31 Getting the current date and time
 
-
 // var rightNow = new Date();
 
 // console.log(rightNow);
 
-
-
-
 // 32 Extracting parts of the date and time
-
 
 // var rightNow = new Date()
 // var day = rightNow.getDay()
@@ -24,11 +19,7 @@
 
 // console.log(month);
 
-
-
-
 // 33 Specifying a date and time
-
 
 // var current = new Date()
 // var selected = new Date("Nov 18, 2007")
@@ -39,22 +30,92 @@
 
 // console.log(result);
 
-
-
-
 // 34 Changing elements of a date and time
 
+// var date = new Date()
+// date.setFullYear("2026")
+// date.setMonth("11")
+// date.setDate("25")
+// date.setHours("10")
+// date.setMinutes("30")
+// date.setSeconds("45")
+// date.setMilliseconds("500")
 
-var date = new Date()
-var fu = date.setFullYear(2007)
-console.log(`year is ${fu}`);
+// console.log(`year is ${date}`);
 
+// 35 functions
 
+// function timer(){
+//     var date = new Date()
+//     var hour = date.getHours()
+//     var minute = date.getMinutes()
+//     var second = date.getSeconds()
+//     var ampm = ""
 
+//     if (hour >= 12){
+//         ampm = "PM"
+//     }else{
+//         ampm = "AM"
+//     }
 
+//     if (hour >= 12){
+//         hour = hour - 12
+//     }else if(hour == 0){
+//         hour = 12;
+//     }
 
+//     document.getElementById("clock").innerHTML = `Current Time: ${hour}:${minute}:${second} ${ampm}`
+// }
+// setInterval(timer, 1000);
 
+// 36 Functions: Passing them data
 
+// function user(greet){
+//     alert(greet)
+// }
+// user("hello how was your day")
+// console.log("good bro thanks");
 
+// 37 Functions: Passing data back from them
 
+// function sum(merch){
+//     var orderTot;
 
+//     if(merch >= 100){
+//         orderTot = merch;
+//     }else if(merch < 50.01){
+//         orderTot = merch + 5
+//     }else{
+//         orderTot = merch + 5 (0.3 * (merch - 50))
+//     }
+//     return orderTot
+// }
+// var result = sum(500)
+// alert(result)
+
+// 38 Functions: Local vs. global variables
+
+// var user = "hello"  // global variable
+// console.log(user);
+
+// function greet(){
+//     var user = "assalamualaikum"    // Local variavble
+//     console.log(user);
+// }
+// greet()
+
+// 39 switch statements: How to start them
+
+// var day = "sunaf";
+
+// switch (day) {
+//   case "sun":
+//     alert("fun day");
+//     break;
+//   case "sat":
+//     alert("masti day");
+//     break;
+//     default :
+
+//     alert("bad day")
+// }
