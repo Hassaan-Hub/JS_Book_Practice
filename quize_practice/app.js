34101d9236c4dfa067eff79df6d1544ad4deddc7
Hassaan Farid <hassaanali994@gmail.com>
2026-02-05 18:22:24 +0500

    practice_work

---
+++ quize_practice/app.js
@@ -0,0 +1,25 @@
+// for loop
+// var i = 1
+// for(var i = 0; i <5; i++){
+//     console.log(i);
+// }
+
+// white loop
+// var i = 1
+// while (i <= 5) {
+//     console.log(i);
+//     i++
+// }
+
+// do while loop
+// var i = 1;
+// do {
+//     console.log(i);
+//     i++
+// }while(i < 5)
+
+
+
+var div = document.getElementsByTagName("div");
+
+console.log(div);
