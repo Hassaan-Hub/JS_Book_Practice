44b42282b3fa287ad0d3d28878d66fd507bb9f1e
Hassaan Farid <hassaanali994@gmail.com>
2026-02-11 23:03:52 +0500

    upated

---
+++ practice_javaScript_51to60/app.js
@@ -0,0 +1,60 @@
+// 51 Reading and setting paragraph text
+
+// function morePara(){
+//     var para = document.getElementById("myParagraph");
+//     para.innerHTML = "This is a Paragraph. This is more paragraph"
+// }
+
+
+
+
+// 52 Manipulating images and text
+
+
+// function chandImage(){
+//     document.getElementById("hideImg").style.display = "none"
+// }
+
+
+
+
+// 53 Swapping images
+
+
+// function chandImage(){
+//     document.getElementById("hideImg").src = "../../All Applications/pokemonio/images/bigImg.gif"
+// }
+
+// function privousImg(){
+//     document.getElementById("hideImg").src = "../../All Applications/pokemonio/images/bigImg.gif1.png"
+// }
+
+
+
+
+// 54 Swapping images and setting classes
+
+
+// function swappingImages(){
+//     var main = document.getElementById("swapImg")
+//     main.style.fontSize = "40px"
+//     main.className = "hassaan"
+// }
+
+
+
+
+// 55 Setting styles
+
+
+// function stylesSetting(){
+//     var main = document.getElementById("stylSet")
+//     main.className += "big"
+// }
+
+// function stylesSetting(){
+//     var main = document.getElementById("stylSet")
+//     main.style.fontSize = "2em"
+// }
+
+
