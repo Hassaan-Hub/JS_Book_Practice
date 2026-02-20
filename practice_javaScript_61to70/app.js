abc6e8ce1dbe6e65863974430a12b7c32ffc82c4
Hassaan Farid <hassaanali994@gmail.com>
2026-02-20 12:36:36 +0500

    updated

--- practice_javaScript_61to70/app.js
+++ practice_javaScript_61to70/app.js
@@ -8,8 +8,32 @@
 
 // 62 The DOM: More ways to target elements
 
+// var div = document.getElementById("main")
+// var pNode = div.parentNode
+// var pChild = div.lastChild
+// console.log(pChild);
 
 
 
 
+// 63 The DOM: Getting a target's name
+
+// var parent = document.getElementById("main")
+// var target = parent.firstChild;
+// nName = target.nodeName
+// nType = target.nodeType
+// console.log(nName);
+
+
+
+
+// 64 The DOM: Counting elements
+
+// var li = document.getElementsByTagName("li")
+// var len = li.length;
+// for(var i = 0; i < len; i++){
+//     if(li[i].innerHTML == ""){
+//         li[i].innerHTML = "coming soon"
+//     }
+// }
 
