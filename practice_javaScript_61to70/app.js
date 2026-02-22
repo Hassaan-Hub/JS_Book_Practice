158abba1633ae212b083af4b8187be1b200e6950
TumharaGitHubName <tumhara@email.com>
2026-02-22 18:08:28 +0500

    updated

--- practice_javaScript_61to70/app.js
+++ practice_javaScript_61to70/app.js
@@ -61,20 +61,3 @@
 
 // var numOfItems = document.getItemById("paragraph").attributes.length
 // console.log(numOfItems);
-
-
-
-
-// prototype
-
-// function Students(name, color){
-//     this.name = name,
-//     this.color = color
-// }
-
-// Students.prototype.greet = function(){
-//     console.log(`my name is ${this.name} my fourite color is ${this.color}`);
-// }
-
-// let card = new Students("hassaan", "red")
-// card.greet()
