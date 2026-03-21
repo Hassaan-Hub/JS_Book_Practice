8acb3258428b22a1a0a1f71dd37a01661f9b5a27
TumharaGitHubName <hassaanali994@gmail.com>
2026-03-21 23:32:25 +0500

    refactor: comment out unused code and improve structure in app.js; add inner div in index.html

--- practice_javaScript_71to80/app.js
+++ practice_javaScript_71to80/app.js
@@ -1,18 +1,46 @@
 // 71 Objects: Methods
 
-var plan1 = {
-    names: {
-        name1: "hassaan",
-        name2: "ali",
-        name3: "JavaScript",
-        name4: "Objects",
-    }
-};
+// var plan1 = {
+//     names: {
+//         name1: "hassaan",
+//         name2: "ali",
+//         name3: "JavaScript",
+//         name4: "Objects",
+//     }
+// };
 
-for (var i in plan1) {
-    // console.log(plan1[i]);
-    for(var j in plan1[i]){
-        console.log(plan1[i][j]);
+// for (var i in plan1) {
+//     for(var j in plan1[i]){
+//         console.log(plan1[i][j]);
         
-    }
-}
+//     }
+// }
+
+
+
+
+// function Plan(name, price, model, company){
+//     this.name = name,
+//     this.price = price,
+//     this.model = model,
+//     this.company = company
+// }
+
+// var namePro = prompt("Enter car name")
+// var pricePro = prompt("Enter car price")
+// var modelPro = prompt("Enter car model")
+// var companyPro = prompt("Enter car company")
+// var result = new Plan(namePro, pricePro, modelPro, companyPro)
+
+// for(var i in result){    
+//     document.getElementById("inner").innerHTML += i + ": " + result[i] + "<br>";
+//     // console.log(result[i]);  
+    
+// }
+
+
+
+
+// 73 Objects: Constructors for methods
+
+    
